f4111dbb57fbfde1c6b607aec0dc96b391cf09ff
joakimolofsson <jjoakim.olofsson@gmail.com>
2019-01-25 23:23:07 +0100

    Login and register

--- routes/api.js
+++ routes/api.js
@@ -3,24 +3,57 @@ const router = express.Router();
 
 import UserModel from '../models/user';
 
-router.get('/', (req, res) => {
-    const newUser = UserModel({
-        firstname: 'Joakim',
-        lastname: 'Olofsson',
-        age: '32',
-        email: 'jjoakim.olofsson@gmail.com',
-        password: 'pass',
-    });
-    
-    newUser.save(function (err, newUser) {
-        if(err) {
-            console.log(err);
+router.post('/', async (req, res) => {
+    try {
+        const loginUser = await UserModel.findOne({
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        if(loginUser != null) {
+            console.log(`Login Success ${req.body.email} ${req.body.password} ${Date()}`);
+            res.send('Login Success');
         } else {
-            console.log(newUser);
+            console.log(`Login Failed ${req.body.email} ${req.body.password} ${Date()}`);
+            res.send('Login Failed');
         }
+    } catch(err) {
+        console.log(`/api/ Error: ${err}`);
+        res.send('Something Went Wrong');
+    }
+});
+
+router.post('/register', async (req, res) => {
+    const newUser = UserModel({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        age: req.body.age,
+        email: req.body.email,
+        password: req.body.password
     });
 
-    res.send('First page');
+    try {
+        const checkDbForSameEmail = await UserModel.findOne({
+            email: newUser.email
+        });
+
+        if(checkDbForSameEmail != null) {
+            console.log(`Email Already Exists: ${newUser.email}`)
+            return res.send('Email Already Exists');
+        }
+    } catch(err) {
+        console.log(`checkDbForSameEmail: ${err}`);
+        return res.send('Something Went Wrong');
+    }
+
+    try {
+        const addNewUserToDb = await newUser.save();
+        console.log(`New User Registered: ${addNewUserToDb}`);
+        res.send('New User Registered');
+    } catch(err) {
+        console.log(`Error Registering New User: ${err}`);
+        res.send('Error Registering New User');
+    }
 });
 
 export default router;
