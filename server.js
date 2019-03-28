871d30ff4ea05ebd1cbfa3947f3b8ec8e067e70d
joakimolofsson <jjoakim.olofsson@gmail.com>
2019-03-28 20:16:39 +0100

    test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const api = require('./routes/api');
 const path = require('path');
 
 dotEnv.config();
-mongoDb();
+/* mongoDb(); */
 
 const app = express(),
 port = process.env.PORT || 5000;
@@ -18,10 +18,12 @@ app.use(express.json());
 app.use(cors());
 app.use('/api', api);
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
 
